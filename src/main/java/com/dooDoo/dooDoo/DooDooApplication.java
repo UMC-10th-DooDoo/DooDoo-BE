8369f43778391e2f3db89b437bc2d49fc530aff5
[timerrrr] <[sichan0304@naver.com]>
2026-07-10 10:17:55 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/dooDoo/dooDoo/DooDooApplication.java
@@ -0,0 +1,13 @@
+package com.dooDoo.dooDoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DooDooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DooDooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dooDoo/dooDoo/DooDooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dooDoo.dooDoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DooDooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
